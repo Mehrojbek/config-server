e92654f943a835c150fc287e2d640a29a2c4ce4d
Mehrojbek <mehrojbekdeveloper@gmail.com>
2025-08-02 18:58:02 +0500

    first

---
+++ config-server/src/test/java/uz/dtc/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.dtc.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
